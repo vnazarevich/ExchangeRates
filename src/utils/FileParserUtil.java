ed03c0b8bc1f7625e34081b504dc869d47a059b5
Vik <vnazarevich@gmail.com>
2016-05-21 17:14:19 +0300

    start with FileParseUtil

--- src/utils/FileParserUtil.java
+++ src/utils/FileParserUtil.java
@@ -1,14 +1,37 @@
 package utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 import com.epam.vik.middleWareServece.models.ExchangeRate;
 
 public class FileParserUtil {
-	public List<ExchangeRate> createOriginalDataList(File file){
-		//BufferedReader reader = new BufferedReader(new FileReader(file));
+	private String fileContent;
+	public List<ExchangeRate> createOriginalDataList(File file) {
+		parseFile(file);
+		createListResult();
+
 		return null;
+	}
+	private void createListResult() {
+		
+		
+	}
+	private void parseFile(File file) {
+		String line = null;
+		StringBuffer stBuf = new StringBuffer();
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+			while ((line = reader.readLine()) != null) {
+				stBuf.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		fileContent = stBuf.toString();		
 	}
 
 }
